4fb0a19785b2de31158f1cd3c84b2e3d48756dbb
Ahloi <xjian.94@gmail.com>
2018-09-28 16:27:00 +0800

    INITIAL COMMIT

---
+++ MemeApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MemeApp
+//
+//  Created by loi xiong jian on 26/09/2018.
+//  Copyright © 2018 loi xiong jian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+
+    
+    
+}
+
